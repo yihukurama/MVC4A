8de22ac2e8d4505809fc9b24e4f240c2a0bf906b
yihukurama <479757405@qq.com>
2016-02-23 15:56:16 +0800

    外置存储器文件管理

---
+++ core/src/main/java/com/yihukurama/core/MVC4A.java
@@ -0,0 +1,16 @@
+package com.yihukurama.core;
+
+import android.content.Context;
+
+import com.yihukurama.core.android.device.sdcard.SDCardManager;
+import com.yihukurama.core.utils.AppInfoUtil;
+
+/**
+ * Created by dengshuai on 16/2/23.
+ */
+public class MVC4A {
+    public static void init(Context context){
+        AppInfoUtil.initAppInfoUtil(context);
+
+    }
+}
